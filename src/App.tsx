917074f231150a1a10092c45b2b7676412159e06
djdjskdjd18874-commits <djdjskdjd18874@gmail.com>
2026-01-17 23:49:14 +0000

    add1

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import ContactUs from './pages/ContactUs'
 import Footer from './componats/Footer'
 
 import Car from './pages/Car'
-import Card from './pages/Card'
+
 import product from './static/product'
 import SignupPage from './pages/SignupPage'
 
@@ -26,7 +26,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/ContactUs" element={<ContactUs />} />
       <Route path="/Car" element={<Car />} />
-     <Route path="/Card" element={<Card  />} />
+     
      <Route path="/SignupPage" element={<SignupPage />} />
      </Routes>
      <Footer/>
